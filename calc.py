261e0b1ed8c517fb2904d493948d2e0a53f91f33
Garrett Scott <Garret-t@users.noreply.github.com>
2021-04-15 16:50:28 -0700

    put values in list

--- calc.py
+++ calc.py
@@ -1,13 +1,8 @@
-#Write the code for subtract
-#Do a commit
-#Write the code for multiply
-#Do a commit
-#Write code for division
-#Do a commit
-
 def calc(a, b):
     sum = a + b
     #print(sum)
     difference = a - b
     multiply = a * b
     divide = a / b
+
+    ops = [sum, difference, multiply, divide]
