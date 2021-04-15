3e8a96cf4b264b2b1c43ce9bc9780d2450a4c98a
Garrett Scott <Garret-t@users.noreply.github.com>
2021-04-15 16:48:23 -0700

    subtract

--- calc.py
+++ calc.py
@@ -1,3 +1,11 @@
+#Write the code for subtract
+#Do a commit
+#Write the code for multiply
+#Do a commit
+#Write code for division
+#Do a commit
+
 def calc(a, b):
     sum = a + b
-    print(sum)
+    #print(sum)
+    difference = a - b
